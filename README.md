e04eb8996b45827e89fbb08aec58be5a4c4a3d71
dainavi.purav <dainavi.purav@cmss.in>
2024-05-19 16:19:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# udemy_008_favorite_chat_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/udemy_008_favorite_chat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.udemy_008_favorite_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
